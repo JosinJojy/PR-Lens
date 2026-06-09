60df9e77a4938c0423e3c0deda2c72bd7d6ee8ba
JosinJojy <josinjojy1@gmail.com>
2026-06-09 17:50:04 +0530

    Setup shadcn with custom theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
+
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,8 +17,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="">
-      <body className="min-h-screen">{children}</body>
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("", "font-sans", geist.variable)}
+    >
+      <body className="min-h-screen">
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem={false}
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
-
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>Hello world</div>
+    <div>
+      <Button>Click me</Button>
+    </div>
   );
 }
