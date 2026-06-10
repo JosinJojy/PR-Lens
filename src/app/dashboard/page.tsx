b94398fa55ea87cb03f099530b4085c5ac09b222
JosinJojy <josinjojy1@gmail.com>
2026-06-10 17:30:20 +0530

    Designed Dashboard layout

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const Dashboard = () => {
+  return (
+    <div>
+      Dashboard content
+    </div>
+  )
+}
+
+export default Dashboard
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,8 @@
-import { Button } from "@/components/ui/button";
-import Logout from "@/module/auth/components/Logout";
 import { requireAuth } from "@/module/auth/utils/auth-utils";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   await requireAuth();
 
-  return <div>
-    Hello World
-    <Logout>
-      <Button>Logout</Button>
-    </Logout>
-  </div>;
+  return redirect('/dashboard')
 }
